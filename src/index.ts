55978f85e5960d88a3cece58f553056714a20d90
david2014betoben <davidgerardonunezrojas@gmail.com>
2026-08-25 17:04:12 -0400

    feat: pedidos actualizado con swagger

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import expres from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import swaggerUi from "swagger-ui-express";
 import { pool } from "./config/db.js";
 import type { Request, Response } from "express";
-import productsRouter from "./routes/pedidos.routes.js";
+import pedidosRouter from "./routes/pedidos.routes.js";
+import path from "node:path";
+import fs from "node:fs";
+
 dotenv.config();
 const app = expres();
 const PORT = process.env.PORT || 3000;
@@ -12,7 +16,15 @@ app.use(cors());
 
 app.use(expres.json());
 
-app.use("/productos", productsRouter);
+app.use("/productos", pedidosRouter);
+
+const swaggerFilePath = path.resolve("./src/swagger-output.json");
+if (fs.existsSync(swaggerFilePath)) {
+  const swaggerDocument = JSON.parse(fs.readFileSync(swaggerFilePath, "utf-8"));
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+} else {
+  console.log("archivo swagger-output.json no encontrado");
+}
 
 app.get("/", function (req: Request, res: Response) {
   res.json({
